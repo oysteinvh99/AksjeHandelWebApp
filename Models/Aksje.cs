cf8594561339f5eed60891c5414b6b3b76fdb275
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-20 14:46:24 +0200

    Lagt til aksjemodell

---
+++ Models/Aksje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AksjeHandelWebApp.Models
+{
+    public class Aksje
+    {
+        public int Id { get; set; }
+        public float Verdi { get; set; }
+        [ForeignKey(typeof(Firma))]
+        [ManyToOne]
+        public Firma FirmaID { get; set; }
+
+
+
+    }
+}
+
