41da0570f147a874943338db05f122cdeda68e56
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-20 16:19:27 +0200

    Update DB.cs

--- Models/DB.cs
+++ Models/DB.cs
@@ -1,11 +1,37 @@
-﻿using System;
+using System;
+using Microsoft.EntityFrameworkCore;
 namespace AksjeHandelWebApp.Models
 {
-    public class DB
+    public class DB:DbContext
+
+
     {
-        public DB()
+        public DB(DbContextOptions<DB> options) : base(options)
         {
+            Datebase.EnsureCreated();
         }
+
+        public virtual DbSet<Person> Personer { get; set; }
+
+        public virtual DbSet<Firma> Firmaer { get; set; }
+
+        public virtual DbSet<Portofolje> Portofoljer { get; set; }
+
+        public virtual DbSet<Ordre> Ordre { get; set; }
+
+        public virtual DbSet<Aksje> Aksjer { get; set; }
+
+        public virtual DbSet<OrdreLinje> Ordrelinjer { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
     }
+    
+    
 }
 
