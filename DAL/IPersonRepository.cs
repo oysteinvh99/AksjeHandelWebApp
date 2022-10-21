3efb68023d5a1c606889da6746ea82ea2a7c0cad
Tarnawskii <70313210+Tarnawskii@users.noreply.github.com>
2022-10-21 14:17:12 +0200

    Update IPersonRepository.cs

--- DAL/IPersonRepository.cs
+++ DAL/IPersonRepository.cs
@@ -7,10 +7,13 @@ namespace AksjeHandelWebApp.DAL
 {
     public interface IPersonRepository
     {
-         Task<Person> hentPerson(int id);
-         Task<List<Aksje>> hentAksjer();
-         Task <Portofolje> hentPortofolje(int id);
+        Task<Person> hentPerson(int id);
+        Task<List<Aksje>> hentAksjer();
+        Task <Portofolje> hentPortofolje(int id);
         Task<int> sjekkPerson(string email);
+        Task<bool>registrerOrder(Ordre innOrder);
+        Task<Firma>hentFirma(int id);
+        Task<bool>slettBruker(int id);
     }
 }
 
