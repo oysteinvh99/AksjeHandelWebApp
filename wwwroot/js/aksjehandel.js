1b0437ca052da141dbbb6ca4eb316cfd17f1f6d6
Tarnawskii <adrian_tar7@hotmail.com>
2022-10-24 13:27:17 +0200

    La til fungerende antall fra antall boksen i topppen av listen

--- wwwroot/js/aksjehandel.js
+++ wwwroot/js/aksjehandel.js
@@ -9,6 +9,7 @@ function hentAlleAksjene() {
 }
 
 function formaterAksjer(Aksjer) {
+    var nr = 0;
     let ut = "<table class='table table-striped'>" +
         "<tr>" +
         "<th>Navn</th><th>Pris</th><th>Antall</th><th>Kjøp</th>" +
@@ -17,9 +18,10 @@ function formaterAksjer(Aksjer) {
         ut += "<tr>" +
             "<td>" + aksje.firma.navn + "</td>" +
             "<td>" + aksje.verdi + "</td>" +
-            "<td>" + "<input type='number' id='antall' style='width: 2.5em'></td>" +
-                "<td> <a class='btn btn-primary' onClick='getInputValue()' href='kjopbekreftelse.html?id=" + aksje.id + "&antall=" + $("#antall").val() + "'>Kjøp</a></td>" +
+            "<td>" + '<input type="number" id="antall-' + aksje.id + '" style = "width: 2.5em" ></td > ' +
+            "<td> <a class='btn btn-primary' href='kjopbekreftelse.html?id=" + aksje.id + "&antall=" + $("#antall").val() + "'>Kjøp</a></td>" +
             "</tr>";
+        nr++;
     }
     ut += "</table>";
     $("#aksjene").html(ut);
