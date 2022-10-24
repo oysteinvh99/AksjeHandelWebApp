35987f704540b4e0303822c75f519423c1c1b2bc
Tarnawskii <adrian_tar7@hotmail.com>
2022-10-24 12:42:19 +0200

    Littt bekreftelse kode og kjopbekreftelse html

--- wwwroot/js/aksjehandel.js
+++ wwwroot/js/aksjehandel.js
@@ -17,10 +17,19 @@ function formaterAksjer(Aksjer) {
         ut += "<tr>" +
             "<td>" + aksje.firma.navn + "</td>" +
             "<td>" + aksje.verdi + "</td>" +
-            "<td>" + "<input type='text' id='antall' style='width: 2.5em'></td>" +
-            "<td> <a class='btn btn-primary' href='index.html?id=" + aksje.id + "'>Kjøp</a></td>" +
+            "<td>" + "<input type='number' id='antall' style='width: 2.5em'></td>" +
+                "<td> <a class='btn btn-primary' onClick='getInputValue()' href='kjopbekreftelse.html?id=" + aksje.id + "&antall=" + $("#antall").val() + "'>Kjøp</a></td>" +
             "</tr>";
     }
     ut += "</table>";
     $("#aksjene").html(ut);
 }
+
+$.get(url, function (id) {
+    if (id != 0) {
+        window.location.href = 'kjopbekreftelse.html?id=' + id + "?antall=" + antall + "?aid=" + aid;
+    }
+    else {
+        window.location.href = 'registrerBruker.html'
+    }
+});
---
+++ wwwroot/js/kjopbekreftelse.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    const aksjeID = params.id;
+    var antall = params.antall;
+    const brukerID = params.bid;
+    print(aksjeID,antall,brukerID)
+});
+
+function print(AID, antall, BID) {
+    $("#aksjeID").html(AID);
+    $("#antall").html(antall);
+    $("#brukerID").html(BID);
+}
