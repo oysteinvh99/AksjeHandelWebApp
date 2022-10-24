12f11e2ba8c372dc688db9ba4e9d64c21b653733
emilhengy <72937159+emilhengy@users.noreply.github.com>
2022-10-24 16:05:37 +0200

    regex på logg inn
    h

--- wwwroot/js/loggInn.js
+++ wwwroot/js/loggInn.js
@@ -1,7 +1,20 @@
-﻿function loggInn() {
+﻿function validerEmail(email) {
+    const regexp = /^[a-zA-ZæøåÆØÅ. \-]{2,20}$/;
+    const ok = regexp.test(email);
+    if(!ok) {
+        $("#feilemail").html("Navnet må bestå av 2 til 20 bokstaver");
+         return false;
+    } else {
+        $("#feilemail").html("");
+    return true;
+     }
+    }
+
+function loggInn() {
     const email = $("#email").val()      // må ha med denne som ikke har blitt endret for å vite hvilken kunde som skal endre
     const url = "Home/sjekkPerson?email=" + email
 
+   
     $.get(url, function (id) {
         if (id != 0) {
             sessionStorage.setItem("id", id);
@@ -12,3 +25,5 @@
         }
     });
 }
+
+
