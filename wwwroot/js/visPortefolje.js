49e7ba889e84741a1b6cbba5d8987fcc80e7b6eb
Tarnawskii <adrian_tar7@hotmail.com>
2022-10-24 18:42:09 +0200

    Lagt til dineAksjer(visPortefolje) html og js

---
+++ wwwroot/js/visPortefolje.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    hentAlleAksjene();
+});
+
+function hentAlleAksjene() {
+    $.get("Home/hentPortefoljeAksjer", function (Aksjer) {
+        formaterAksjer(Aksjer);
+    });
+}
+
+function formaterAksjer(Aksjer) {
+    let ut = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>Navn</th><th>Pris</th><th>Antall</th><th>Kjøp</th>" +
+        "</tr>";
+    for (let aksje of Aksjer) {
+        ut += "<tr>" +
+            "<td>" + aksje.firma.navn + "</td>" +
+            "<td>" + aksje.verdi + "</td>" +
+            "<td>" + aksje.antall + "</td>" +
+            "</tr>";
+    }
+    ut += "</table>";
+    $("#aksjene").html(ut);
+}
