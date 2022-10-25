7b09f4d849f4f14d7f4627f921c5dcb5f1530abd
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-25 11:26:32 +0200

    hgjghj

--- wwwroot/js/visPortefolje.js
+++ wwwroot/js/visPortefolje.js
@@ -4,23 +4,25 @@
 
 function hentAlleAksjene() {
     const url = "Home/visPortefolje?id=" + sessionStorage.getItem("id");
-    $.get(url, function (nyVisning)) {
-        f
+    $.get(url, function (visPortefolje) {
+        formaterAksjer(visPortefolje)
 
     });
 
-function formaterAksjer(nyVisning) {
-    let ut = "<table class='table table-striped'>" +
-        "<tr>" +
-        "<th>Navn</th><th>Antall</th><th>Verdi</th>" +
-        "</tr>";
-    for (let visning of nyVisning) {
-        ut += "<tr>" +
-            "<td>" + visning.aksje.firma.navn + "</td>" +
-            "<td>" + visning.antall + "</td>" +
-            "<td>" + visning.verdi + "</td>" +
+
+    function formaterAksjer(visPortefolje) {
+        let ut = "<table class='table table-striped'>" +
+            "<tr>" +
+            "<th>Navn</th><th>Antall</th><th>Verdi</th>" +
             "</tr>";
+        for (let visning of visPortefolje) {
+            ut += "<tr>" +
+                "<td>" + visning.aksje.firma.navn + "</td>" +
+                "<td>" + visning.antall + "</td>" +
+                "<td>" + visning.verdi + "</td>" +
+                "</tr>";
+        }
+        ut += "</table>";
+        $("#aksjene").html(ut);
     }
-    ut += "</table>";
-    $("#aksjene").html(ut);
 }
