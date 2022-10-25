5f2ef0dd57b662b53c5f65661b8c07d937d2ef3e
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-25 16:42:51 +0200

    Gjort Endringer på min side

--- wwwroot/js/minSide.js
+++ wwwroot/js/minSide.js
@@ -1,37 +1,20 @@
 $(function () {
-    hentAlleAksjene();
+    hentEnPerson();
 });
 
-function hentAlleAksjene() {
-    const url = "Home/visPortefolje?id=" + sessionStorage.getItem("id");
-    $.get(url, function (visPortefolje) {
-        formaterAksjer(visPortefolje)
+function hentEnPerson() {
+    const url = "Home/hentPerson?id=" + sessionStorage.getItem("id");
+    $.get(url, function (person) {
+        formaterPerson(person)
        
     });
 
 
-    function formaterAksjer(visPortefolje) {
-        let totalAntall = 0;
-        let totalVerdi = 0;
-        let ut = "<table class='table table-striped'>" +
-            "<tr>" +
-            "<th>Navn</th><th>Antall</th><th>Verdi</th>" +
-            "</tr>";
-        for (let visning of visPortefolje) {
-            ut += "<tr>" +
-                "<td>" + visning.aksje.firma.navn + "</td>" +
-                "<td>" + visning.antall + "</td>" +
-                "<td>" + visning.verdi + "</td>" +
-                "</tr>";
-            totalAntall = totalAntall + parseInt(visning.antall);
-            totalVerdi = totalVerdi + parseInt(visning.verdi);
-        }
-        ut += "<tr>" +
-            "<td>" + "Total:" + "</td>" +
-            "<td>" + totalAntall + "</td>" +
-            "<td>" + totalVerdi + "</td>" +
-            "</tr>";
-        ut += "</table>";
-        $("#aksjene").html(ut);
+    function formaterPerson(person) {
+        $("#fornavn").html(person.fornavn)
+        $("#etternavn").html(person.etternavn)
+        $("#email").html(person.email)
+        $("#telefon").html(person.telefon)
+      
     }
 }
