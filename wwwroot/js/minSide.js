b8145f4ff67b35e7cf082cf63e2980e8a01b125e
Tarnawskii <70313210+Tarnawskii@users.noreply.github.com>
2022-10-25 12:59:13 +0200

    Update minSide.js

--- wwwroot/js/minSide.js
+++ wwwroot/js/minSide.js
@@ -13,11 +13,12 @@ function hentAlleAksjene() {
     function formaterAksjer(visPortefolje) {
         let ut = "<table class='table table-striped'>" +
             "<tr>" +
-            "<th>Navn</th><th>Antall</th><th>Verdi</th>" +
+            "<th>Navn</th><th>Aksjeverdi</th><th>Antall</th><th>Verdi</th>" +
             "</tr>";
         for (let visning of visPortefolje) {
             ut += "<tr>" +
                 "<td>" + visning.aksje.firma.navn + "</td>" +
+                "<td>" + visning.aksje.verdi + "</td>" +
                 "<td>" + visning.antall + "</td>" +
                 "<td>" + visning.verdi + "</td>" +
                 "</tr>";
