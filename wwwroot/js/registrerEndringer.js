4a90d0c02f7c71270d06d28c03f1d00b3787eb4a
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-25 16:59:14 +0200

    Endret endringer

---
+++ wwwroot/js/registrerEndringer.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    hentEnPerson();
+});
+
+function hentEnPerson() {
+    const url = "Home/hentPerson?id=" + sessionStorage.getItem("id");
+    $.get(url, function (person) {
+        formaterPerson(person)
+
+    });
+
+
+    function formaterPerson(person) {
+        $("#fornavn").val(person.fornavn)
+        $("#etternavn").val(person.etternavn)
+        $("#email").val(person.email)
+        $("#telefon").val(person.telefon)
+
+    }
+}
+
