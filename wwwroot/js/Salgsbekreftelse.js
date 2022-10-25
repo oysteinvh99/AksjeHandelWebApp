bb34b83ab3bc002648d888516e8e2f380daad98a
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-25 19:52:10 +0200

    Oppdatert salg

--- wwwroot/js/Salgsbekreftelse.js
+++ wwwroot/js/Salgsbekreftelse.js
@@ -29,42 +29,64 @@ function formatter(Aksje, antall, AID) {
 
 //Registrere ordre, kjøp
 function bekreftOrdre() {
+   
+
+
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const AID = params.id;
     var antall = 0;
     antall = parseInt(params.antall);
+    var sjekk = 0;
+        const url = "Home/visPortefolje?id=" + sessionStorage.getItem("id");
+    $.post(url, function (visPortefolje) {
+        for (let visning of visPortefolje) {
+            if (parseInt(visning.antall) > antall && visning.aksje.id == AID) {
+                registrerOrdre();
+            }
+           
 
-    console.log(AID);
+        }
+       
+     
+    });
+    
 
-    const url = "Home/hentAksje?id=" + AID;
-    $.get(url, function (Aksjen) {
-        const url = "Home/hentPortefolje?id=" + sessionStorage.getItem("id");
-        $.post(url, function (portefolje) {
-            //Hentet fra nettet, datetime
-            var currentdate = new Date();
-            var datetime = currentdate.getDay() + "/" + currentdate.getMonth()
-                + "/" + currentdate.getFullYear() + "  "
-                + currentdate.getHours() + ":"
-                + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-            console.log(datetime)
+    function registrerOrdre() {
+        const url = "Home/hentAksje?id=" + AID;
+        $.get(url, function (Aksjen) {
+            const url = "Home/hentPortefolje?id=" + sessionStorage.getItem("id");
+            $.post(url, function (portefolje) {
+                //Hentet fra nettet, datetime
+                var currentdate = new Date();
+                var datetime = currentdate.getDay() + "/" + currentdate.getMonth()
+                    + "/" + currentdate.getFullYear() + "  "
+                    + currentdate.getHours() + ":"
+                    + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+                console.log(datetime)
 
-            console.log("antall:" + antall + " AID:" + AID + "Aksjen.ID: " + Aksjen.id)
-            var Ordre = {
-                Dato: datetime,
-                Type: false,
-                AntallAksjer: antall,
-                Aksje: Aksjen,
-                Portefolje: portefolje
-            };
+                console.log("antall:" + antall + " AID:" + AID + "Aksjen.ID: " + Aksjen.id)
+                var Ordre = {
+                    Dato: datetime,
+                    Type: false,
+                    AntallAksjer: antall,
+                    Aksje: Aksjen,
+                    Portefolje: portefolje
+                };
 
-            $.post("Home/registrerOrdre", Ordre, function (registrert) {
-                if (registrert) {
-                    window.location.href = 'aksjehandel.html';
-                } else {
-                    //Noe gikk feil
-                }
+                $.post("Home/registrerOrdre", Ordre, function (registrert) {
+                    if (registrert) {
+                        window.location.href = 'aksjehandel.html';
+                    } else {
+                        //Noe gikk feil
+                    }
+                });
             });
         });
-    });
+    }
+    $("#feil").html("Du har ikke så mange aksjer")
+
+    
+       
+    
 }
