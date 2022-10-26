b6221253014db9a87dfa58151c4d7b1e6f4d4590
oysteinvh99 <74008924+oysteinvh99@users.noreply.github.com>
2022-10-26 14:24:53 +0200

    Fiksa på regEx

--- wwwroot/js/loggInn.js
+++ wwwroot/js/loggInn.js
@@ -2,25 +2,29 @@
     const regexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     const ok = regexp.test(email);
     if(!ok) {
-        $("#feilEmail").html("Mailen må være skrevet i riktig format");
          return false;
     } else {
-        $("#feilEmail").html("");
-    return true;
+        return true;
      }
     }
 
 function loggInn() {
-    const email = $("#email").val()      // må ha med denne som ikke har blitt endret for å vite hvilken kunde som skal endre
-    const url = "Home/sjekkPerson?email=" + email
+    const email = $("#email").val()// må ha med denne som ikke har blitt endret for å vite hvilken kunde som skal endre
+    const url = "Home/sjekkPerson?email=" + email;
+    const ok = validerEmail(email);
+    if (ok) {
+        $.get(url, function (id) {
+            if (id != 0) {
+                sessionStorage.setItem("id", id);
+                window.location.href = 'indexLoggetInn.html'
+            }
+            else {
+                window.location.href = 'registrerBruker.html'
+            }
+        });
+    }
+    else {
+        $("#feilEmail").html("Mailen må være skrevet i riktig format");
 
-    $.get(url, function (id) {
-        if (id != 0) {
-            sessionStorage.setItem("id", id);
-            window.location.href = 'indexLoggetInn.html'
-        }
-        else {
-            window.location.href = 'registrerBruker.html'
-        }
-    });
+    }
 }
