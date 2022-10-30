81a726164452cc2e069f349f86c2f5cd4532a369
Tarnawskii <70313210+Tarnawskii@users.noreply.github.com>
2022-10-30 17:10:22 +0100

    Update minSide.js

--- wwwroot/js/minSide.js
+++ wwwroot/js/minSide.js
@@ -2,14 +2,13 @@ $(function () {
     hentEnPerson();
 });
 
+//Henter en gitt person utifra session id
 function hentEnPerson() {
     const url = "Home/hentPerson?id=" + sessionStorage.getItem("id");
     $.get(url, function (person) {
         formaterPerson(person)
        
     });
-
-
     function formaterPerson(person) {
         $("#fornavn").html(person.fornavn)
         $("#etternavn").html(person.etternavn)
@@ -18,24 +17,26 @@ function hentEnPerson() {
       
     }
 }
+
+//Slett bruker funksjon som sletter brukeren
 function slettBruker() {
     const url = "Home/slettBruker?id=" + sessionStorage.getItem("id");
     $.get(url, function (ok) {
         if (ok) {
             loggUt();
         }
-
-
-
     });
 
 }
 
+//Logg ut funksjon som logger ut brukeren
 function loggUt() {
     sessionStorage.clear("id");
     window.location.href = 'index.html';
 
 }
+
+//Funksjon som sender brukeren til visOrdre og viser alle ordrene til brukeren
 function seOrdre() {
     window.location.href = 'visOrdre.html';
 
